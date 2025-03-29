b54ea2c842c0903593686d7abfb97a1c463abc40
David Skyberg <davidskyberg@gmail.com>
2025-03-29 10:46:23 -0400

    u/rundevelopment insights

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,25 @@
 pub trait Decimals {
     /// Round the fractional part of a float to `fract_decimals`.  This is not simply a
     /// string trim function.
-    fn decimals(&self, fract_decimals: u32) -> Self;
+    fn round(&self, num_decimals: u32) -> Self;
 }
 
 impl Decimals for f32 {
-    fn decimals(&self, fract_decimals: u32) -> Self {
-        let y = 10i32.pow(fract_decimals) as f32;
+    fn round(&self, num_decimals: u32) -> Self {
+        // from u/rundevelopment: f32 and f64 can only represent at most about 8 and 15 decimals respectively.
+        // So clamp fract_decimals to that to prevent 10i32.pow(fract_decimals) from overflowing
+        let num_decimals = num_decimals.clamp(0, 8);
+        let y = 10i32.pow(num_decimals) as f32;
         (*self * y).round() / y
     }
 }
 
 impl Decimals for f64 {
-    fn decimals(&self, fract_decimals: u32) -> Self {
-        let y = 10i64.pow(fract_decimals) as f64;
+    fn round(&self, num_decimals: u32) -> Self {
+        // from u/rundevelopment: f32 and f64 can only represent at most about 8 and 15 decimals respectively.
+        // So clamp fract_decimals to that to prevent 10i32.pow(fract_decimals) from overflowing
+        let num_decimals = num_decimals.clamp(0, 15);
+        let y = 10i64.pow(num_decimals) as f64;
         (*self * y).round() / y
     }
 }
@@ -26,7 +32,7 @@ mod tests {
 
     #[test]
     fn test_decimals() {
-        assert_eq!(1.23456789.decimals(2), 1.23);
-        assert_eq!(1.23456789.decimals(3), 1.235);
+        assert_eq!(1.23456789.round(2), 1.23);
+        assert_eq!(1.23456789.round(3), 1.235);
     }
 }
